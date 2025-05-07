ea063d516af1feba15ccd93a2d5c2ff0dc677d71
Sushant Aryal <sushantaryal05@gmail.com>
2025-05-07 15:50:33 -0500

    lists and tuples

---
+++ lists/slicing.py
@@ -0,0 +1,12 @@
+list1=[1,2,3,4,5,6,7]
+list1[0:3]
+
+print(list1)
+print (list1[0:3])
+
+
+tuple(0,1,2,3,4,5)
+tuple[0]
+
+#cannot rassign values in tuple like tuple[0]=3
+#but you can add tuples like tuple1+tuple2
