58a6f7149a3a13b44d90cb17b81292d972ed957e
Sushant Aryal <sushantaryal05@gmail.com>
2025-05-10 19:57:37 -0500

    functions basic

---
+++ functions, methods and namespaces/basic_builtin_functions.py
@@ -0,0 +1,9 @@
+list1 = [10, 1,2,3,4,5]
+len(list1) #returns length of list1
+
+list1.append[6] #adds 6 at the end of list1
+
+sorted(list1) #returns the sorted version of list1 but does not change original list
+
+list1.sort() #sorts the original list
+
---
+++ strings.py
@@ -0,0 +1,6 @@
+string1 = 'hello, my github stalker'
+string2 = "as you can see, i'm tired of vibe coding, 'so i'm refreshing my python fundamentals'"
+string3 = """being good is not enough \n I have to be great""" #triple quote is used for multi line string
+
+string1[0] #first character of string1
+#they can also be sliced and added, etc like lists and tuples
