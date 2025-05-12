32781cddf7403fad3adaed3b5690e6521be70cce
Sushant Aryal <sushantaryal05@gmail.com>
2025-05-11 21:01:28 -0500

    default

---
+++ functions, methods and namespaces/custom_functions.py
@@ -0,0 +1,9 @@
+def add(x,y):
+    sum = x+y
+    return sum
+
+def abs(x):
+    if x>0:
+        return x
+    else:
+        return -x
---
+++ functions, methods and namespaces/default_and_keyword_arguments.py
@@ -0,0 +1,7 @@
+def exceeds_threshold(value, threshold = 0.):
+    if value > threshold:
+        return True
+    else:
+        return False
+
+exceeds_threshold(2)
