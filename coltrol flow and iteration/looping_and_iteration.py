0b273ae15197f7f425182c2557b4e67c686ea58c
Sushant Aryal <sushantaryal05@gmail.com>
2025-05-11 21:47:05 -0500

    looping and iteration

---
+++ coltrol flow and iteration/looping_and_iteration.py
@@ -0,0 +1,26 @@
+#for loop, loops for an specified number of times or over an items that contains multiple items you want to access
+for element in [1, 10, 100, 1000]:
+    # element takes values of 1, 10, 100, 1000
+    pass
+
+for key, value in adict.items():
+    # iterate through key-value pairs in adict
+    pass
+
+for i in range(0, N):
+    # i starts at 0 and ends at N-1
+    pass
+
+#while loop, loops until some sort of condition is met then exits
+while x > 0:
+    # do something as long as x > 0
+    pass
+
+while element in aset:
+    # do something as long as element in aset
+    pass
+
+while True:
+    # do something forever
+    pass
+
