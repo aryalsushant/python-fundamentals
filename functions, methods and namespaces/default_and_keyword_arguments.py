7f973232642023cc28c030a5537d9f5f43918be7
Sushant Aryal <sushantaryal05@gmail.com>
2025-05-11 21:05:46 -0500

    keyword arguments

--- functions, methods and namespaces/default_and_keyword_arguments.py
+++ functions, methods and namespaces/default_and_keyword_arguments.py
@@ -5,3 +5,13 @@ def exceeds_threshold(value, threshold = 0.):
         return False
 
 exceeds_threshold(2)
+
+print(sum([1,2,3])) #prints sum of 1, 2 and 3
+print(sum([1,2,3],100)) #100 is now a start value and it will be added to the sum
+
+exceeds_threshold(threshold =100, value =2)
+#if you specify which is what, order does not matter. 
+#this is called KEYWORD ARGUMENT
+#if you provide a keyword for one place, you have to do it for all places
+
+
